3e0d365aea43ac6e2f362f54b9719694f3994dd2
code404it <jessikahd.smith@gmail.com>
2020-08-04 13:33:21 -0400

    update kid card

--- src/components/KidCard.js
+++ src/components/KidCard.js
@@ -5,12 +5,12 @@ import { removeKid } from '../actions/fetchKids'
 const KidCard = ({kid, removeKid}) => {
   return (
     <div className="kid-card">
-      <span id="kid-name">{kid.name}</span>Make Room For More Kids:<span id="Make-Room-For-More-Kids">{kid.name}</span> <br />
-      Date:<span id="date">{kid.date}</span> <br />
-      <span id="title">{kid.title}</span>What Did They Do:<span id="What-Did-They-Do">{kid.title}</span> <br />
-      <span id="description">{kid.description}</span>Details:<span id="Details">{kid.details}</span> <br />
-      <span id="rating">{kid.rating}</span>Rating:<span id="Rating">{kid.rating}</span> <br />
-      <span id="discipline-score">{kid.discipline_score}</span>Discipline Score<span id="Discipline-Score">{kid.score}</span> <br />
+     Name: <span id="kid-name">{kid.name}</span><br />
+      Date (DD/MM/YYYY): <span id="date">{kid.date}</span> <br />
+      What Did They Do: <span id="title">{kid.title}</span> <br />
+      Details: <span id="description">{kid.description}</span><br />
+      Rating (1 to 5): <span id="Rating">{kid.rating}</span> <br />
+      Discipline Score (1-100): <span id="Discipline-Score">{kid.discipline_score}</span> <br />
       <button id="delete-button" onClick={() => removeKid(kid.id)}>Remove {kid.name}?</button>
       <br /><br />
     </div>
