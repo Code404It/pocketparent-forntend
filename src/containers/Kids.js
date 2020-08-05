0ded9f5658681f8447629cdeeb821ccf14157add
code404it <jessikahd.smith@gmail.com>
2020-08-05 07:28:20 -0400

    took out code I wasn't using

--- src/containers/Kids.js
+++ src/containers/Kids.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import KidCard from '../components/KidCard'
 import { connect } from 'react-redux'
 import { fetchKids } from '../actions/fetchKids'
-//import AddKidButton from '../components/AddKidButton'
+
 
 
 class Kids extends Component {
@@ -14,7 +14,6 @@ class Kids extends Component {
   render() {
     return(
       <div className="kids-container">
-        {/* < AddKidButton name="Add Kid" addKid={this.props.addKid}/> */}
         {this.props.kids.map((k, id) => < KidCard key={id} kid={k} />)}
       </div>
     )
@@ -27,12 +26,7 @@ const mapStateToProps = state => {
   }
 }
 
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     fetchKids: () => dispatch(fetchKids()),
-//     //addKid: kid => dispatch(addKid(kid))
-//   }
-// }
+
 
 
 
